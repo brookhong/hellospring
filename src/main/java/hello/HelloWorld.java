120653cd37d447b7b94c8bb3df92a661b86e9cb8
brook hong <hzgmaxwell@hotmail.com>
2015-03-10 16:01:55 +0800

    Bean Definition Inheritance; init-method; destroy-method; BeanPostProcessor

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,19 @@
 package hello;
 
 public class HelloWorld {
-   private String message;
+    private String message;
 
-   public void setMessage(String message){
-      this.message  = message;
-   }
+    public void setMessage(String message){
+        this.message  = message;
+    }
 
-   public void getMessage(){
-      System.out.println("Your Message : " + message);
-   }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+    public void init(){
+        System.out.println("Bean is going through init.");
+    }
+    public void destroy(){
+        System.out.println("Bean will destroy now.");
+    }
 }
--- src/main/java/hello/MainApp.java
+++ src/main/java/hello/MainApp.java
@@ -1,14 +1,24 @@
 package hello;
 
-import org.springframework.context.ApplicationContext;
+// import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+// import org.springframework.beans.factory.xml.XmlBeanFactory;
+// import org.springframework.core.io.ClassPathResource;
+
 public class MainApp {
-   public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+        // XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+        // HelloWorld objA = (HelloWorld) factory.getBean("helloWorld");
 
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        objA.setMessage("I'm object A");
+        objA.getMessage();
+        HelloWorld objB = (HelloWorld) context.getBean("helloChina");
+        objB.getMessage();
 
-      obj.getMessage();
-   }
+        context.registerShutdownHook();
+    }
 }
